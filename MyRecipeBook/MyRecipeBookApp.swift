c89466a50e2373d274007e20190c5a90bd3a0cf3
April Zhang <aprilzh23@gmail.com>
2024-08-20 12:26:26 -0700

    Initial Commit

---
+++ MyRecipeBook/MyRecipeBookApp.swift
@@ -0,0 +1,18 @@
+//
+//  MyRecipeBookApp.swift
+//  MyRecipeBook
+//
+//  Created by April (Huaiyu) Zhang on 8/20/24.
+//  
+//
+
+import SwiftUI
+
+@main
+struct MyRecipeBookApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
