35ec4ff97a39d49ad2c89dfdd937e53cc0b7b0fe
April Zhang <aprilzh23@gmail.com>
2024-08-20 14:53:28 -0700

    Add recipe list view model and utils

---
+++ MyRecipeBook/Utils/Constants.swift
@@ -0,0 +1,15 @@
+//
+//  Constants.swift
+//  MyRecipeBook
+//
+//  Created by April (Huaiyu) Zhang on 8/20/24.
+//  
+//
+
+import Foundation
+
+struct Constants {
+    struct Urls {
+        static let recipeCategoriesURL = URL(string: "www.themealdb.com/api/json/v1/1/categories.php")!
+    }
+}
---
+++ MyRecipeBook/View Models/RecipeCategoryListViewModel.swift
@@ -0,0 +1,47 @@
+//
+//  RecipeCategoryListViewModel.swift
+//  MyRecipeBook
+//
+//  Created by April (Huaiyu) Zhang on 8/20/24.
+//  
+//
+
+import Foundation
+
+@MainActor
+class RecipeCategoryListViewModel: ObservableObject {
+    
+    @Published var recipeCategories: [RecipeCategoryViewModel] = []
+    
+    func populateCategories() async {
+        do {
+            let recipeCategoriesResponse = try await NetworkManager().get(url: Constants.Urls.recipeCategoriesURL) { data in
+                return try? JSONDecoder().decode(RecipeCategoryResponse.self, from: data)
+            }
+            
+            self.recipeCategories = recipeCategoriesResponse.categories.map(RecipeCategoryViewModel.init)
+            
+        } catch {
+            print(error)
+        }
+    }
+}
+
+struct RecipeCategoryViewModel: Identifiable {
+    
+    let id = UUID()
+    private let recipeCategory: RecipeCategory
+    
+    init(_ recipeCategory: RecipeCategory) {
+        self.recipeCategory = recipeCategory
+    }
+    
+    var title: String {
+        recipeCategory.strCategory
+    }
+    
+    var imageUrl: URL? {
+        URL(string: recipeCategory.strCategoryThumb)
+    }
+    
+}
