98c8748cbf2ed6c083425c1914794890e8edb376
April Zhang <aprilzh23@gmail.com>
2024-08-22 20:35:00 -0700

    Clean up some notes

--- MyRecipeBook/Network/NetworkManager.swift
+++ MyRecipeBook/Network/NetworkManager.swift
@@ -8,10 +8,6 @@
 
 import Foundation
 
-// getAllCategories
-// getRecipeForCategory
-// getRecipeDeatils
-
 enum NetworkError: Error {
     case badRequest(statusCode: Int)
     case decodingError
@@ -24,10 +20,6 @@ class NetworkManager {
     func get<T: Decodable>(url: URL, parse: (Data) -> T?) async throws -> T {
         
         let (data, response) = try await URLSession.shared.data(from:url)
-        
-//        if (response as? HTTPURLResponse)?.statusCode != 200 {
-//            throw NetworkError.badRequest
-//        }
         
         guard let httpResponse = response as? HTTPURLResponse else {
             throw NetworkError.invalidResponse
